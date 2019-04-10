3cb0f22ae5c42dc29e7d5b12ba42d8a7d4823899
Sharon Kawale <c3297125@uon.edu.au>
2019-04-10 13:47:55 +0000

    read and print input text to console

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-int main () {
-    
-   printf("woohoo");
-   
-return 0;
+
+int main() {
+	FILE *input;
+	input = fopen("input.txt", "r");
+	
+	while(feof(input) ==0 ) {
+	    char c;
+	    //read a character
+	    fscanf(input, "%c", &c);
+	    
+	   
+	    //print that character to the console
+	    printf("%c\n", c);
+	    
+	}
+	return 0;
 }
